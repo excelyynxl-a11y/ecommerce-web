b0816382151e90369bb5a5ed9f45d7f41791ef0a
excel <excelyynxl@gmail.com>
2025-12-18 15:23:56 +0800

    create navbar component

---
+++ components/navbar.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { useCartStore } from "@/store/cart-store"
+import { ShoppingBagIcon, Bars3Icon, XMarkIcon} from "@heroicons/react/24/outline"
+import Link from "next/link"
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+
+export const Navbar = () => {
+
+    const { items } = useCartStore();
+    const cartCount = items.reduce((acc, item) => acc + item.quantity, 0);
+
+    const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 768) {
+                setMobileOpen(false);
+            }
+        }
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [])
+
+    return (
+        <nav className="sticky top-0 z-50 bg-white shadow">
+            <div className="container mx-auto flex items-center justify-between px-10 py-4">
+                <Link href="/" className="hover:text-blue-600">
+                    Buy Until Im Broke 
+                </Link>
+                <div className="hidden md:flex space-x-10">
+                    <Link href="/">
+                        Home
+                    </Link>
+                    <Link href="/products" className="hover:text-blue-600">
+                        Products
+                    </Link>
+                    <Link href="/checkout" className="hover:text-blue-600">
+                        Checkout
+                    </Link>
+                </div>
+                <div className="flex items-center space-x-4">
+                    <Link href="/checkout" className="relative">
+                        <ShoppingCartIcon className="h-6 w-6"/>
+                        {cartCount > 0 && (
+                            <span 
+                                className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-xs text-white"
+                            > 
+                                {cartCount}
+                            </span>
+                        )}
+                    </Link>
+
+                    <Button 
+                        variant="ghost" 
+                        className="md:hidden"
+                        onClick={() => setMobileOpen((prev) => !prev)}
+                    >
+                        {mobileOpen ? 
+                            <XMarkIcon className="h-6 w-6"/> 
+                            : 
+                            <Bars3Icon className="h-6 w-6"/>
+                        }
+                    </Button>
+                </div>
+            </div>
+
+            {mobileOpen && (
+                <nav className="md:hidden bg-white shadow-md">
+                    <ul className="flex flex-col p-4 space-y-2">
+                        <li>
+                            <Link href="/" className="block hover:text-blue-600">
+                                Home
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link href="/products" className="block hover:text-blue-600">
+                                Products
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link href="/checkout" className="block hover:text-blue-600">
+                                Checkout
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+        </nav>
+    )
+}
